ec349ba35c801ec0e3d72c67f8385b80bbf9ff31
maispormenos <luke.novaes86@gmail.com>
2025-10-08 12:12:23 +0100

    updated main code and added count words function file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-path = "/home/spikesama/workspace/github.com/lucas/bookbot/books/frankenstein.txt"
-
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
+file_path = "/home/spikesama/workspace/github.com/lucas/bookbot/books/frankenstein.txt"
 
 def main():
-    content = get_book_text(path)
-    print (content)
+    content = get_book_text(file_path)
+    words = get_num_words(content)
+    print (f"Found {words} total words")
+
+def get_book_text(p):
+    with open(p) as f:
+        return f.read()
+    
+from stats import get_num_words
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(c):
+    w = c.split()
+    return len(w)
