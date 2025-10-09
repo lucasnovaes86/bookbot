a4b7489d6557ea002a34b1e33acd28f37b94627f
maispormenos <luke.novaes86@gmail.com>
2025-10-09 13:42:19 +0100

    "added new sorting function to stats.py"

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-file_path = "/home/spikesama/workspace/github.com/lucas/bookbot/books/frankenstein.txt"
+# Importa funcoes do arquivo stats.py       
+from stats import get_num_words, get_num_char, get_sorted_char
 
-def main():
-    content = get_book_text(file_path)
-    words = get_num_words(content)
-    print (f"Found {words} total words")
+file_path = "books/frankenstein.txt"
 
+# Recebe o caminho do arquivo e abre ele como texto
 def get_book_text(p):
     with open(p) as f:
         return f.read()
     
-from stats import get_num_words
+def main():
+    text = get_book_text(file_path)
+    num_words = get_num_words(text)
+    counted_characters = get_num_char(text)
+    sorted_characters = get_sorted_char(counted_characters)
+
+
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {file_path}...")
+    print ("----------- Word Count ----------")
+    print (f"Found {num_words} total words")
+    print ("--------- Character Count -------")
+    
+    for c in sorted_characters:
+        if c["char"].isalpha():
+            print(f"{c["char"]}: {c["count"]}")
+
+    print ("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,33 @@
+# Recebe o texto completo do documento, separa as palavras, e conta o número de palavras
 def get_num_words(c):
     w = c.split()
     return len(w)
+
+
+# Recebe o texto, converte para 'lower case', adiciona e conta caracteres do texto
+def get_num_char(c):
+    num_characters = {}    
+    lower_case_text = c.lower()
+    for character in lower_case_text:
+        if character not in num_characters:
+            num_characters[character] = 0
+        num_characters[character] += 1
+    return num_characters
+
+
+# Distribui a contagem de caracteres em uma lista e ordena decrescente
+def get_sorted_char(characters):
+    sorted_characters = []
+
+    for character in characters:
+        temp_dict = {
+            "char": character,
+            "count": characters[character]
+            }
+        sorted_characters.append(temp_dict)
+
+    def get_num(item):
+        return item["count"]
+
+    sorted_characters.sort(reverse = True, key = get_num)
+    return sorted_characters
